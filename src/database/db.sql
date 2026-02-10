38cd196458cada640cbe3bbd91528a6013260d5a
Kajlongero <dev.kajlongero@gmail.com>
2026-02-10 03:21:17 -0400

    feat: db initial config

---
+++ src/database/db.sql
@@ -0,0 +1,38 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TYPE rotation_reason_enum AS ENUM('AUTOMATIC', 'FORCED');
+
+CREATE TYPE job_status_enum AS ENUM('IDLE', 'RUNNING', 'FAILED');
+
+CREATE TABLE
+  keys (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    kid TEXT NOT NULL UNIQUE,
+    jwk JSONB NOT NULL,
+    ttl INTEGER NOT NULL,
+    enc_pk BYTEA NOT NULL,
+    version INTEGER NOT NULL DEFAULT 1,
+    is_active BOOLEAN DEFAULT TRUE,
+    rotation_reason rotation_reason_enum NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    deletable_at TIMESTAMP WITH TIME ZONE NOT NULL
+  );
+
+CREATE TABLE
+  jobs (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4 (),
+    ttl INTEGER NOT NULL,
+    type TEXT NOT NULL UNIQUE,
+    status job_status_enum DEFAULT 'IDLE',
+    counter INTEGER DEFAULT 0,
+    version INTEGER DEFAULT 1,
+    last_run_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    next_run_at TIMESTAMP WITH TIME ZONE NOT NULL
+  );
+
+CREATE INDEX idx_keys_expires_at ON keys (expires_at);
+
+CREATE INDEX idx_keys_deletable_at ON keys (deletable_at);
+
+CREATE INDEX idx_jobs_next_run_at ON jobs (next_run_at);
---
+++ src/database/index.ts
@@ -0,0 +1,43 @@
+import type { Selectable, Insertable, Updateable } from "kysely";
+
+import type { ColumnType, Generated, JSONColumnType } from "kysely";
+
+import { ECSignJWK, OKPSignJWK, RSASignJWK } from "../types/jwks.type";
+
+export interface Database {
+  keys: KeysTable;
+  jobs: JobsTable;
+}
+
+export interface KeysTable {
+  id: Generated<string>;
+  kid: string;
+  jwk: JSONColumnType<RSASignJWK | ECSignJWK | OKPSignJWK>;
+  ttl: number;
+  enc_pk: Buffer;
+  version: number;
+  is_active: boolean;
+  rotation_reason: "AUTOMATIC" | "FORCED";
+  created_at: ColumnType<Date, string | Date, never>;
+  expires_at: ColumnType<Date, string | Date, never>;
+  deletable_at: ColumnType<Date, string | Date, never>;
+}
+
+export interface JobsTable {
+  id: Generated<string>;
+  ttl: number;
+  type: string;
+  status: "IDLE" | "RUNNING" | "FAILED";
+  counter: number;
+  version: number;
+  last_run_at: ColumnType<Date, Date | string, Date | string>;
+  next_run_at: ColumnType<Date, Date | string, Date | string>;
+}
+
+export type Keys = Selectable<KeysTable>;
+export type NewKeys = Insertable<KeysTable>;
+export type UpdateKeys = Updateable<KeysTable>;
+
+export type Jobs = Selectable<JobsTable>;
+export type NewJobs = Insertable<JobsTable>;
+export type UpdateJobs = Updateable<JobsTable>;
