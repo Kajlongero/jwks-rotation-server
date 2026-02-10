2e8346d5c0d50d02ac749582dcbe99ac2c9d2b50
Kajlongero <dev.kajlongero@gmail.com>
2026-02-10 03:27:23 -0400

    initial method config and importing on main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import express from "express";
 
 import { ENV_CONFIG } from "./configs/envs";
 
+import { JobsVault } from "./modules/jobs-vault";
+
 const app = express();
 
 (async () => {
+  await JobsVault.getInstance().schedule();
+
   app.listen(ENV_CONFIG.PORT, () => {
     `App running at port: ${ENV_CONFIG.PORT}`;
   });
--- src/modules/jobs-vault/index.ts
+++ src/modules/jobs-vault/index.ts
@@ -1,1 +1,21 @@
-export class JobsVault {}
+export class JobsVault {
+  static instance: JobsVault;
+
+  private constructor() {
+    if (JobsVault.instance) return JobsVault.instance;
+
+    return this;
+  }
+
+  static getInstance() {
+    if (!JobsVault.instance) JobsVault.instance = new JobsVault();
+
+    return JobsVault.instance;
+  }
+
+  async schedule() {}
+
+  async start() {
+    while (true) {}
+  }
+}
