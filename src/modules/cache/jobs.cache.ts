bddcd1931496efddb99cc0a9aa4cbdf6faaa0e17
Kajlongero <dev.kajlongero@gmail.com>
2026-02-10 19:12:18 -0400

    feat: adding methods on JwksCache and JobsCache

--- src/modules/cache/jobs.cache.ts
+++ src/modules/cache/jobs.cache.ts
@@ -1,4 +1,6 @@
 import NodeCache from "node-cache";
+import { JOBS_CACHE_KEY } from "./keys/jobs-cache.keys";
+import { Jobs } from "../../database";
 
 export class JobsCache {
   private static instance: JobsCache;
@@ -15,5 +17,18 @@ export class JobsCache {
     if (!JobsCache.instance) JobsCache.instance = new JobsCache();
 
     return JobsCache.instance;
+  }
+
+  retrieve(): Jobs | undefined {
+    if (this.cache.has(JOBS_CACHE_KEY))
+      return this.cache.get(JOBS_CACHE_KEY) as Jobs;
+  }
+
+  save(job: Jobs, ttl: number = 0) {
+    this.cache.set(JOBS_CACHE_KEY, job, ttl);
+  }
+
+  clear() {
+    this.cache.del(JOBS_CACHE_KEY);
   }
 }
--- src/modules/cache/jwks.cache.ts
+++ src/modules/cache/jwks.cache.ts
@@ -1,5 +1,14 @@
 import NodeCache from "node-cache";
 
+import {
+  JWKS_SET,
+  JWKS_ACTIVE_KEY,
+  JWKS_PUBLIC_KEY,
+  JWKS_PRIVATE_KEY,
+} from "./keys/jwks-cache.keys";
+
+import type { Keys } from "../../database";
+
 export class JwksCache {
   private static instance: JwksCache;
 
@@ -15,5 +24,26 @@ export class JwksCache {
     if (!JwksCache.instance) JwksCache.instance = new JwksCache();
 
     return JwksCache.instance;
+  }
+
+  retrieve(): Keys[] | undefined {
+    if (this.cache.has(JWKS_SET)) return this.cache.get(JWKS_SET) as Keys[];
+  }
+
+  save(keys: Keys[]) {
+    const publicJwk = keys.map((k) => k.jwk);
+
+    keys.forEach((k) => {
+      this.cache.set(JWKS_PRIVATE_KEY(k.kid), {
+        kid: k.kid,
+        enc_pk: k.enc_pk,
+        isActive: k.is_active,
+      });
+
+      if (k.is_active) this.cache.set(JWKS_ACTIVE_KEY, k);
+    });
+
+    this.cache.set(JWKS_SET, keys);
+    this.cache.set(JWKS_PUBLIC_KEY, publicJwk);
   }
 }
