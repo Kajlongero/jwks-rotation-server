725360003893412ac1301d4beb4eb60265575f54
Kajlongero <dev.kajlongero@gmail.com>
2026-02-11 01:28:42 -0400

    feat: connecting on main.ts the apirouter

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,15 @@ import express from "express";
 import { ENV_CONFIG } from "./configs/envs";
 
 import { JobsVault } from "./modules/jobs-vault";
+import { ApiRouter } from "./routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+ApiRouter(app);
 
 (async () => {
   const vault = JobsVault.getInstance();
